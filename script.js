2e41b9ae1c0386f7773f5ab1e14c4d4ee5cc9a73
Roberto Erick <robertoerickam@gmail.com>
2023-06-24 15:00:15 -0600

    clase03 completada

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ console.log(btn);                                           //imprimir la funcio
  const createTask = (evento) => {                           //Se crea un listener de un clic, con la funcion del evento que se va a realizar
     evento.preventDefault();                                //Evita que se recargue la pagina al dar clic en el boton y que se mantenga lo que se mand a la consola
     const input = document.querySelector("[data-form-input]");//registra lo que esta en el input y lo coloca en una constante
-    console.log(input.value);                               //despues imprimimos en la conosla el valor del input que se agrego
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+    input.value = '';
+    //backticks
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content;
+
+    list.appendChild(task);
+    console.log(content);                               //despues imprimimos en la conosla el valor del input que se agrego
 }
 
 //Listener
