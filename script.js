442593814a5618056aa7cf958c6c528b250991ca
Roberto Erick <robertoerickam@gmail.com>
2023-06-27 19:36:36 -0600

    clase04 completada

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-const btn = document.querySelector("[data-form-btn]");      //crear una variable a partir de eñ selector que esta en el HTML
+(() => {
+const btn = document.querySelector('[data-form-btn]');      //crear una variable a partir de eñ selector que esta en el HTML
 
-console.log(btn);                                           //imprimir la funcion en la consola, el valor del boton 
-
+//console.log(btn);                                           //imprimir la funcion en la consola, el valor del boton 
 //Funcion cuando se hae un clic
+
 const createTask = (evento) => {                           //Se crea un listener de un clic, con la funcion del evento que se va a realizar
     evento.preventDefault();                                //Evita que se recargue la pagina al dar clic en el boton y que se mantenga lo que se mand a la consola
-    const input = document.querySelector("[data-form-input]");//registra lo que esta en el input y lo coloca en una constante
+    const input = document.querySelector('[data-form-input]');//registra lo que esta en el input y lo coloca en una constante
     const value = input.value;                              //el valor colocado en el input, se coloca en en la costante para js
-    const list = document.querySelector("[data-list]");     //Seleccionar la lista en la constante list  
-    const task = document.createElement("li");              //Crear cada elemento de una lista con la constante task
-    task.classList.add("card");                             //hacerlo con el formato card de CSS
+    const list = document.querySelector('[data-list]');     //Seleccionar la lista en la constante list  
+    const task = document.createElement('li');              //Crear cada elemento de una lista con la constante task
+    task.classList.add('card');                             //hacerlo con el formato card de CSS
     input.value = '';                                       //limpiar el input 
     //backticks
-    const content = `<div>                                  
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-    </div>
+    const taskContent = document.createElement('div');      //Crea el elemento div 
+    
+    const titleTask = document.createElement('span');       //y crea el contenido que debe de tener este 
+    titleTask.classList.add('task');
+    titleTask.innerText = value;                            //coloca el texto que escribio el usuario
+    taskContent.appendChild(checkComplete());               //el simbolo de check para la tarea
+    taskContent.appendChild(titleTask);                     //la tarea que escribio el usuario y que esta cyardada en titletask
+    
+    const content = `
     <i class="fas fa-trash-alt trashIcon icon"></i>`;       //Codigo HTML para crear un elemento nuevo en la lista con el valor que se tenga en input
-    task.innerHTML = content;                               //Funcion para agregar el codigo HTML escrito anteriormente
-
+    // task.innerHTML = content;                            //Funcion para agregar el codigo HTML escrito anteriormente
+    task.appendChild(taskContent);                          //contenido de la tarea
     list.appendChild(task);                                 //Colocar como hijo de task
-    console.log(content);                               //despues imprimimos en la conosla el valor del input que se agrego
+    
+    //console.log(content);                               //despues imprimimos en la conosla el valor del input que se agrego
 }
 
 //Listener
@@ -34,4 +41,17 @@ const createTask = (evento) => {                           //Se crea un listener
 //Arrow functions o funciones anonimas
 btn.addEventListener('click', createTask);                  //Cuando se de unn click, vamos a ir a la funcion crate Task
 
+const checkComplete = () => {                               //esta funcion sirve para que cuando se presione el boton check cambie a azul
+    const i = document.createElement('i');
+    i.classList.add('far', 'fa-check-square', 'icon');
+    i.addEventListener('click', completeTask);
+    return i;
+};
 
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon');
+    element.classList.remove('far');
+};
+})();                                                      //Todo se encierra dentro de una funcion y se llama al instante para evitar que este al alcance del usuario
